ef7ab45a89c07c5572913bf11a85b2ab173b0afb
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-07-24 21:45:25 +0100

    document algorithm

--- check_status.py
+++ check_status.py
@@ -9,14 +9,17 @@ import json
 import os
 import subprocess
 
+## CONSTANTS
 HERE = os.path.dirname(__file__) or "."
 DATA = os.path.join(HERE, "api.json")
 # status.sh - ping
 # status.py - GPIO button press
 STATUS = os.path.join(HERE, "status.py")
 
+## ALGORITHM
 subprocess.call(["git", "pull"], cwd=HERE)
 
+# load the data AFTER the pull
 with open(DATA) as f:
     data = json.load(f)
 
